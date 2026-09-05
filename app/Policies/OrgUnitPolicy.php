bb9c9908e500a4d7ac591ca8cc33b91433c5eaa2
TemitokpeShalom <jshalom067@gmail.com>
2026-09-05 19:13:03 +0100

    Update OrgUnitPolicy.php

--- app/Policies/OrgUnitPolicy.php
+++ app/Policies/OrgUnitPolicy.php
@@ -41,6 +41,16 @@ class OrgUnitPolicy
         return $this->hasManagingAffectationOverDescendantsOrSelf($user, $orgUnit);
     }
 
+    /**
+     * Gestion des cultes (services) : meme regle que la gestion des
+     * membres - il faut un role habilite a gerer des personnes
+     * (can_manage_users), sur ce noeud ou un de ses ancetres.
+     */
+    public function manageCultes(User $user, OrgUnit $orgUnit): bool
+    {
+        return $this->hasManagingAffectationOverDescendantsOrSelf($user, $orgUnit);
+    }
+
     private function hasAffectationOverridingDescendantsOrSelf(User $user, OrgUnit $orgUnit): bool
     {
         return $user->activeAffectations()
