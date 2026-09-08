2ab0b95f4c6dc2da44fedbe997e3ba495302889f
TemitokpeShalom <jshalom067@gmail.com>
2026-09-08 05:01:00 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,6 +10,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RoleSeeder::class,
+            PlanSeeder::class,
             HelpArticlesSeeder::class,
         ]);
     }
