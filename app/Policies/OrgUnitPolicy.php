4458da60c0521a6401635415242c01daa9f6a3db
TemitokpeShalom <jshalom067@gmail.com>
2026-09-08 13:52:46 +0100

    Add files via upload

--- app/Policies/OrgUnitPolicy.php
+++ app/Policies/OrgUnitPolicy.php
@@ -74,6 +74,17 @@ class OrgUnitPolicy
     }
 
     /**
+     * Gestion des equipes et du benevolat (point 08), y compris
+     * l'affectation des membres a une equipe : meme regle que la gestion
+     * des membres et des cultes - il faut un role habilite a gerer des
+     * personnes (can_manage_users), sur ce noeud ou un de ses ancetres.
+     */
+    public function manageTeams(User $user, OrgUnit $orgUnit): bool
+    {
+        return $this->hasManagingAffectationOverDescendantsOrSelf($user, $orgUnit);
+    }
+
+    /**
      * Gestion des finances (mouvements et rapport d'activites) : meme
      * regle que la gestion des membres et des cultes - il faut un role
      * habilite a gerer des personnes (can_manage_users), sur ce noeud ou
